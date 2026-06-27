c43e1d996311d1cbb82cb18d570850c283fc37f9
hyeonglvl <hyeonglvl@gmail.com>
2026-06-27 15:25:59 +0900

    개발 서버 모바일 접속 IP 추가 및 next.config 설정 형식 정리

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  allowedDevOrigins: ["192.168.0.9", "192.168.0.18"],
+  allowedDevOrigins: ["192.168.0.9", "192.168.0.18", "172.29.43.172"],
 };
 
 export default nextConfig;
